bdfc1c5dca28a32e8e78bcbce3eb3937c7dd8896
Davide Giuseppe Farella <4face91@protonmail.com>
2020-03-09 21:29:58 +0100

    Fixed dates

--- data/remote/src/commonMain/kotlin/studio/forface/covid/data/remote/mapper/fieldApiModelMappers.kt
+++ data/remote/src/commonMain/kotlin/studio/forface/covid/data/remote/mapper/fieldApiModelMappers.kt
@@ -1,6 +1,8 @@
 package studio.forface.covid.data.remote.mapper
 
+import com.soywiz.klock.DateFormat
 import com.soywiz.klock.DateTime
+import com.soywiz.klock.parse
 import studio.forface.covid.domain.entity.Id
 import studio.forface.covid.domain.entity.Location
 import studio.forface.covid.domain.entity.Name
@@ -20,9 +22,11 @@ internal class LocationApiModelMapper : ApiModelMapper<LocationParams, Location>
 data class LocationParams(val lat: Double, val lng: Double)
 
 internal class TimestampApiModelMapper : ApiModelMapper<String, DateTime> {
-    override fun String.toEntity() = DateTime.parse(this).local
+    override fun String.toEntity() = DATE_FORMAT.parse(this).local
+    private companion object { val DATE_FORMAT = DateFormat("yyyy-MM-dd'T'HH:mm:ssZ") }
 }
 
 internal class UnixTimeApiModelMapper : ApiModelMapper<Int, DateTime> {
-    override fun Int.toEntity() = DateTime(this.toLong())
+    override fun Int.toEntity() = DateTime(this * A_SEC_IN_MS)
+    private companion object { const val A_SEC_IN_MS = 1000L }
 }
